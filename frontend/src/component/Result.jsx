ea69ee1703cae71b07cd5aa57e49aee10a04d6a4
Osas706 <92319443+Osas706@users.noreply.github.com>
2024-04-20 00:23:41 +0100

    func

--- frontend/src/component/Result.jsx
+++ frontend/src/component/Result.jsx
@@ -1,10 +1,12 @@
-import axios from "axios";
+
 import React, { useEffect, useState } from "react";
 import CopyToClipboard from "react-copy-to-clipboard";
 
 const Result = ({ inputValue }) => {
   const [shortLink, setShortLink] = useState("");
   const [longLink, setLongLink] = useState("");
+  const [urlLink, setUrlLink] = useState("");
+  const [redirectLink, setRedirectLink] = useState("");
   const [copied, setCopied] = useState(false);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
@@ -24,21 +26,37 @@ const Result = ({ inputValue }) => {
       });
 
       const data = await res.json();
+      console.log(data);
+
       setShortLink(data.shortUrl);
       setLongLink(data.longUrl);
+      setUrlLink(data.urlCode);
 
     } catch (error) {
       console.log(error);
       setError(error);
-      
+
     } finally {
       setLoading(false);
+    }
+  };
+
+  const directLink = async () => {
+    try {
+      const res = await fetch(`http://localhost:5000/${urlLink}`)
+      const data = await res.json();
+
+      setRedirectLink(data);
+      console.log(redirectLink);
+    } catch (error) {
+      console.log(error);
     }
   };
 
   useEffect(() => {
     if (inputValue.length) {
       fetchData();
+      directLink();
     }
   }, [inputValue]);
 
@@ -50,7 +68,6 @@ const Result = ({ inputValue }) => {
     return () => clearTimeout(timer);
   }, [copied]);
 
-  //console.log(shortLink);
 
   if(loading){
     return <p className="noData">Loading...</p>
@@ -66,7 +83,7 @@ const Result = ({ inputValue }) => {
           <a href={longLink} target="_blank">{shortLink}</a>
           {/*<a>{shortLink}</a>*/}
 
-          <CopyToClipboard text={shortLink} onCopy={() => setCopied(true)}>
+          <CopyToClipboard text={redirectLink} onCopy={() => setCopied(true)}>
             <button className={copied ? "copied" : ""}>Copy</button>
           </CopyToClipboard>
         </div>
